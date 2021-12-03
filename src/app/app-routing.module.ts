e10e5cdb1cb0b8bb97e63b0aa2299247be80e15c
Renomariocosme <renomario.cosme@gmail.com>
2021-12-03 12:25:33 -0300

    @Renomariocosme criação dos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
+import { CadastroEmpreededorComponent } from './cadastro-empreededor/cadastro-empreededor.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo: "login", pathMatch: "full"},
+  {path: "login", component: LoginComponent},
+  {path:"cadastro-cliente", component: CadastroClienteComponent},
+  {path:'cadastro-empreededor',component: CadastroEmpreededorComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/cadastro-empreededor/cadastro-empreededor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-empreededor',
+  templateUrl: './cadastro-empreededor.component.html',
+  styleUrls: ['./cadastro-empreededor.component.css']
+})
+export class CadastroEmpreededorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
